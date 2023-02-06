54b81debd7e84eed118362bd3f259abb8513beac
Thomas Storey <thomasjstoreymail@gmail.com>
2023-02-06 10:29:19 +0100

    minor changes

--- src/components/StatusPod/StatusPod.js
+++ src/components/StatusPod/StatusPod.js
@@ -3,13 +3,7 @@ import { Button, ProgressBar } from 'react-bootstrap'
 import "./StatusPod.css"
 
 
-function StatusPod({ statusPod, setSelectedPod, setShowPodModal}) {
-
-  const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  }
-
-  // var placeHolderValue;
+function StatusPod({ statusPod, setSelectedPod, setShowPodModal }) {
 
   const [placeHolderValue, setplaceHolderValue] = useState(0)
 
@@ -18,62 +12,59 @@ function StatusPod({ statusPod, setSelectedPod, setShowPodModal}) {
     setShowPodModal(true)
   }
 
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  }
+
   useEffect(() => {
     setplaceHolderValue(getRandomInt(4))
-  },[])
-
-  // const reduced = statusPod.pods //bind this to the aggregated status codes
-  // lets say fully-operational is 10
-  //semi-operational is 5
-  // offline is 0
-
-  // console.log(reduced)
+  }, [])
 
   return (
     <div className='status-pod'>
       <h2 className=''>{statusPod.parent}</h2>
       <p>{statusPod.pods.length} Services</p>
-      <br/>
+      <br />
       <p>
-      Overall health status:
-      <br/> <br/>
+        Overall health status:
+        <br /> <br />
         {
-        placeHolderValue === 1 ? ' Fully Operational' : 
-        placeHolderValue === 2 ? ' Semi Operational' :
-        placeHolderValue === 3 ? ' Downtime' : 
-        " Unknown"
+          placeHolderValue === 1 ? ' Fully Operational' :
+            placeHolderValue === 2 ? ' Semi Operational' :
+              placeHolderValue === 3 ? ' Downtime' :
+                " Unknown"
         }
       </p>
-      
+
       <ProgressBar now={
         placeHolderValue === 1 ? 100 :
-        placeHolderValue === 2 ? 75 :
-        placeHolderValue === 3 ? 50 :
-        placeHolderValue === 4 ? 25 :
-        0
-      } 
-      min={0}
-      max={100}
-      animated 
-      className={
-        placeHolderValue === 1 ? 'progress status-live' : 
-        placeHolderValue === 2 ? 'progress-warning ' :
-        placeHolderValue === 3 ? 'progress-offline ' : 
-        "progress-unknown"
+          placeHolderValue === 2 ? 75 :
+            placeHolderValue === 3 ? 50 :
+              placeHolderValue === 4 ? 25 :
+                0
       }
+        min={0}
+        max={100}
+        animated
+        className={
+          placeHolderValue === 1 ? 'progress status-live' :
+            placeHolderValue === 2 ? 'progress-warning ' :
+              placeHolderValue === 3 ? 'progress-offline ' :
+                "progress-unknown"
+        }
 
-      striped variant={
-        placeHolderValue == 1 ? 'success' : 
-        placeHolderValue == 2 ? 'warning' :
-        placeHolderValue == 3 ? 'danger' : 
-          "info"
-        }  
+        striped variant={
+          placeHolderValue == 1 ? 'success' :
+            placeHolderValue == 2 ? 'warning' :
+              placeHolderValue == 3 ? 'danger' :
+                "info"
+        }
       />
 
       <br />
-        <Button className="shadow btn" variant="light" onClick={loadModal}>
-          View components
-        </Button>
+      <Button className="shadow btn" variant="light" onClick={loadModal}>
+        View components
+      </Button>
     </div>
 
   )
